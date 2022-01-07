45bf5035cfe349dc77a5ce35a95d555c907411c7
Filip Cizkovsky <filous.doma@volny.cz>
2022-01-07 18:13:27 +0100

    getFLagEmoji snippet

--- handy_functions.js
+++ handy_functions.js
@@ -235,3 +235,15 @@ function wait(ms) {
     console.log(num);
   })
   console.log('Done')
+
+
+/// flag emoji
+  function getFlagEmoji(countryCode) {
+    const codePoints = countryCode
+      .toUpperCase()
+      .split('')
+      .map(char =>  127397 + char.charCodeAt());
+    return String.fromCodePoint(...codePoints);
+  }
+
+  getFlagEmoji('US')
