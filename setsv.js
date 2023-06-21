ac20cb971d21dbc93a51d3b27a333efa16d93064
Filip Cizkovsky <filous.doma@volny.cz>
2023-06-21 10:30:21 +0200

    setsv comment

--- setsv.js
+++ setsv.js
@@ -6,6 +6,7 @@
  })();
 
 
+
  /* hinte setsv */
 (() => { 
     var consent = Didomi.getUserStatus().consent_string;
@@ -13,6 +14,6 @@
     img.src = `https://a.centrum.cz/cent/SETSV/testing=filip/gdpr=1/consent=${consent}`;
  })();
 
-
+ /* POZOR, pokud nechci prepsat puvodni hodnoty, tak pouzit testing=+hodnota */
 
 
